dcb26596951b7008c4daeaeb491ee60f0a0ac10d
JawadAhmed07 <qwertykey41@gmail.com>
2024-10-27 14:52:14 +0500

    dark light mode added

--- src/app/(admin)/admin/layout.js
+++ src/app/(admin)/admin/layout.js
@@ -1,33 +1,38 @@
+import { ModeToggle } from "@/components/mode-toggle"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import Link from "next/link"
 
 
-export default function Layout({children}){
-    return (
-        <Tabs defaultValue="account" className="w-full">
-  <TabsList className="w-full">
-    <Link href={`/admin/dashboard`}>
-    <TabsTrigger value="dashboard">Dashboard</TabsTrigger>
-    </Link>
-    <Link href={`/admin/courses`}>
-    <TabsTrigger value="courses">Course</TabsTrigger>
-    </Link>
-    <Link href={`/admin/trainers`}>
-    <TabsTrigger value="trainers">Trainers</TabsTrigger>
-    </Link>
-    <Link href={`/admin/students`}>
-    <TabsTrigger value="students">Students</TabsTrigger>
-    </Link>
-    <Link href={`/admin/batches`}>
-    <TabsTrigger value="batches">Batch</TabsTrigger>
-    </Link>
-  </TabsList>
-  <TabsContent value="dashboard">{children}</TabsContent>
-  <TabsContent value="courses">{children}</TabsContent>
-  <TabsContent value="trainers">{children}</TabsContent>
-  <TabsContent value="students">{children}</TabsContent>
-  <TabsContent value="batches">{children}</TabsContent>
-</Tabs>
+export default function Layout({ children }) {
+  return (
+    <Tabs defaultValue="account" className="w-full">
+      <TabsList
+        TabsList className="w-full justify-around">
+        <div>
+        <Link href={`/admin/dashboard`}>
+          <TabsTrigger value="dashboard">Dashboard</TabsTrigger>
+        </Link>
+        <Link href={`/admin/courses`}>
+          <TabsTrigger value="courses">Course</TabsTrigger>
+        </Link>
+        <Link href={`/admin/trainers`}>
+          <TabsTrigger value="trainers">Trainers</TabsTrigger>
+        </Link>
+        <Link href={`/admin/students`}>
+          <TabsTrigger value="students">Students</TabsTrigger>
+        </Link>
+        <Link href={`/admin/batches`}>
+          <TabsTrigger value="batches">Batch</TabsTrigger>
+        </Link>
+        </div>
+        <ModeToggle />
+      </TabsList>
+      <TabsContent value="dashboard">{children}</TabsContent>
+      <TabsContent value="courses">{children}</TabsContent>
+      <TabsContent value="trainers">{children}</TabsContent>
+      <TabsContent value="students">{children}</TabsContent>
+      <TabsContent value="batches">{children}</TabsContent>
+    </Tabs>
 
-    )
+  )
 }
