5a51152cf0f52aaf51badb77965066163ddd7692
Jawad Ahmed <qwertykey41@gmail.com>
2024-11-03 19:33:54 +0500

    .env file push in vercel
    .env file push in vercel

--- src/app/api/user/login/route.js
+++ src/app/api/user/login/route.js
@@ -3,39 +3,41 @@ import { UserModal } from "@/lib/modals/UserModal";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-
 export async function POST(request) {
-    await connectDb();
-    const obj = await request.json();
-    // console.log("users=>", obj);
-    // console.log("user testing=>", obj)
+  await connectDb();
+  const obj = await request.json();
 
-    const user =await  UserModal.findOne({ email: obj.email });
-    if (!user)
-        return Response.json({
-            message: "User not found ",
-            error: true
-        },{
-            status:404
-        })
+  const user = await UserModal.findOne({ email: obj.email });
+  if (!user)
+    return Response.json(
+      { error: true, msg: "User Not Found" },
+      {
+        status: 404,
+      }
+    );
 
-        var token = jwt.sign({ id:user._id,role: user.role }, process.env.JWT_KEY);
+  const isPasswordMatch = await bcrypt.compare(obj.password, user.password);
 
+  if (!isPasswordMatch)
+    return Response.json(
+      { error: true, msg: "Password is not valid" },
+      {
+        status: 400,
+      }
+    );
 
-    return Response.json({
-        error: false,
-        msg: "User Login successfully",
-        user: user,
-        token: token,
+  const token = jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_KEY
+  );
+
+  return Response.json(
+    {
+      error: false,
+      msg: "User Login Successfully",
+      user,
+      token,
     },
-{
-    status:200
-})
-
-    //     const isPasswordMatch=await bcrypt.compare(obj.password, user.password)
-    //     console.log("isPasswordMatch=>",isPasswordMatch)
-    // return Response.json("User login successful")
-
+    { status: 200 }
+  );
 }
-
-
