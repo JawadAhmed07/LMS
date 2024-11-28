6006cf2e105336fbb6aefc32f9127fe055fac2e0
Jawad Ahmed <qwertykey41@gmail.com>
2024-11-28 06:50:40 +0500

    api

--- src/app/api/batches/route.js
+++ src/app/api/batches/route.js
@@ -1,23 +1,33 @@
 import { connectDb } from "@/lib/dbConnect";
-import { BatchModel } from "@/lib/modals/BatchModal";
+import { BatchModal } from "@/lib/modals/BatchModal";
 
+export async function POST(request) {
+  await connectDb();
+  const obj = await request.json();
+  let newBatch = new BatchModal({ ...obj });
+  newBatch = await newBatch.save();
 
-
-export async function POST(request){
-    await connectDb();
-    const obj = await request.json();
-    let newBatch= new BatchModel({...obj});
-    newBatch = await newBatch.save();
-
-    return Response.json({
-        error:false,
-        msg:"batch added successfully",
-        batch:newBatch,
-    })
+  return Response.json({
+    error: false,
+    msg: "Batch Added Successfully",
+    batch: newBatch,
+  });
 }
 
 export async function GET(req) {
-    await connectDb();
-    const reqUrl = req.url;
+  await connectDb();
+  const reqUrl = req.url;
+  const { searchParams } = new URL(reqUrl);
+  const query = {};
+  if (searchParams.get("course")) {
+    query.course = searchParams.get("course");
+  }
 
+//   console.log("query=>", query);
+const batches = await BatchModal.find(query).populate('course', 'title');
+  return Response.json({
+    error: false,
+    msg: "Batched Fetched Successfully",
+    batches,
+  });
 }
