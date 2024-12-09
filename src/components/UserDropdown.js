425753d558d0dca67b9a05d5f1d6844d6ebbb373
Jawad Ahmed <qwertykey41@gmail.com>
2024-12-09 23:13:21 +0500

    update

--- src/components/UserDropdown.js
+++ src/components/UserDropdown.js
@@ -7,6 +7,7 @@ import Link from "next/link";
 
 export default function UserDropdown({ session }) {
   return session ? (
+    // miantaining github
     <DropdownMenu>
       <DropdownMenuTrigger>
         <Avatar>
