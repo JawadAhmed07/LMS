a7669fe7f16467e227d0d692e838ce4ba3589933
Jawad Ahmed <qwertykey41@gmail.com>
2024-12-10 23:48:40 +0500

    deploy error

--- src/components/UserDropdown.js
+++ src/components/UserDropdown.js
@@ -7,7 +7,6 @@ import Link from "next/link";
 
 export default function UserDropdown({ session }) {
   return session ? (
-    // miantaining github
     <DropdownMenu>
       <DropdownMenuTrigger>
         <Avatar>
